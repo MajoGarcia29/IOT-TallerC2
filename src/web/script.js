905073e88ea4babf6fb1904a5ee6e09d1186d7d2
MajoGarcia29 <majo290701@gmail.com>
2025-09-29 09:14:01 -0500

    Update script.js

--- src/web/script.js
+++ src/web/script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Interfaz WiFi cargada correctamente");
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", () => {
+    alert("Credenciales enviadas, reinicia el ESP32.");
+  });
+});
 
